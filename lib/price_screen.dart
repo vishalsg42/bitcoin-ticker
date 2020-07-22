4c79543cb8758daffb689fc12f827434deac826a
guptavishalprdxn <vishalsg@prdxn.com>
2020-07-22 11:57:15 +0530

    configured android PICKER & IOS picker

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:io' show Platform;
+
+import 'package:bitcoin_ticker/coin_dart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
 
@@ -8,6 +12,69 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   var emojiParser = EmojiParser();
+  String selectedCurrency;
+
+  DropdownButton getAndroidPicker() {
+    List<DropdownMenuItem<String>> currencyDropdown = [];
+    for (int index = 0; index < currenciesList.length; index++) {
+      String currency = currenciesList[index];
+      currencyDropdown.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+    }
+    return DropdownButton<String>(
+      value: selectedCurrency ?? currenciesList[0] ?? 'USD',
+      style: TextStyle(
+        color: Colors.black,
+      ),
+      items: currencyDropdown,
+      onChanged: (String value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+      },
+    );
+  }
+
+//  List<DropdownMenuItem> getDropdownItems() {
+////    currenciesList
+//    List<DropdownMenuItem<String>> currencyDropdown = [];
+//    for (int index = 0; index < currenciesList.length; index++) {
+//      String currency = currenciesList[index];
+//      currencyDropdown.add(
+//        DropdownMenuItem(
+//          child: Text(currency),
+//          value: currency,
+//        ),
+//      );
+//    }
+//    return currencyDropdown;
+//  }
+
+  CupertinoPicker getIOSPicker() {
+    List<Text> currencyDropdown = [];
+    for (int index = 0; index < currenciesList.length; index++) {
+      String currency = currenciesList[index];
+      currencyDropdown.add(
+        Text(
+          currency,
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      children: currencyDropdown,
+      onSelectedItemChanged: (value) {
+        print(value);
+      },
+      itemExtent: 32.0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -43,9 +110,10 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
             Container(
               height: 150.0,
+              alignment: Alignment.center,
               padding: EdgeInsets.only(bottom: 30.0),
-              color: Colors.lightBlueAccent,
-              child: null,
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? getIOSPicker() : getAndroidPicker(),
             )
           ],
         ),
